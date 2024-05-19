15eb2b035227e8fbac58a27ccd4adecc5c06dbaa
Diego Pahua Silvan <dpahuas@gmail.com>
2024-05-19 17:23:42 -0600

    feat: Add Zig Software Foundation sponsorship section

--- astro.config.mjs
+++ astro.config.mjs
@@ -85,6 +85,10 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'Zig Software Foundation',
+					autogenerate: { directory: 'zsf' },
+				},
 			],
 		}),
 	],
